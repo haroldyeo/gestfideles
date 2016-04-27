5c9730d323227e2679c4acd736650d068613f42c
Harold <Harold@Harold-PC>
2016-04-27 23:32:07 +0000

    ajout de tout le code de sidebar, mais pas encore fonctionnel

--- gestFideles/src/controllers/SideBarController.java
+++ gestFideles/src/controllers/SideBarController.java
@@ -1,11 +1,21 @@
 package controllers;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.event.SerializableEventListener;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Row;
 import org.zkoss.zul.Rows;
+
+import services.SidebarPage;
+import services.SidebarPageConfig;
 
 public class SideBarController extends SelectorComposer<Component> {
 
@@ -18,7 +28,7 @@ public class SideBarController extends SelectorComposer<Component> {
     @Wire
     Grid fnList;
 
-    //services
+  //services
     SidebarPageConfig pageConfig = new SidebarPageConfigChapter2Impl();
 
     @Override
@@ -29,9 +39,38 @@ public class SideBarController extends SelectorComposer<Component> {
         Rows rows = fnList.getRows();
 
         for(SidebarPage page:pageConfig.getPages()){
-            Row row = constructSidebarRow(page.getLabel(),page.getIconUri(),page.getUri());
+            Row row = constructSidebarRow(page.getName(), page.getLabel(),page.getIconUri(),page.getUri());
             rows.appendChild(row);
         }
     }
+
+    private Row constructSidebarRow(String name,String label, String imageSrc, final String locationUri) {
+
+        //construct component and hierarchy
+        Row row = new Row();
+        Image image = new Image(imageSrc);
+        Label lab = new Label(label);
+
+        row.appendChild(image);
+        row.appendChild(lab);
+
+        //set style attribute
+        row.setSclass("sidebar-fn");
+
+        //create and register event listener
+        EventListener<Event> actionListener = new SerializableEventListener<Event>() {
+            private static final long serialVersionUID = 1L;
+
+            public void onEvent(Event event) throws Exception {
+                //redirect current url to new location
+                Executions.getCurrent().sendRedirect(locationUri);
+            }
+        };
+
+        row.addEventListener(Events.ON_CLICK, actionListener);
+
+        return row;
+    }
+
 
 }

78b8e4ddff89ea030d4e392f58e8cf25a6b3decf
Harold YEO <harold.yeo@sndi.ci>
2016-04-28 08:05:19 +0000

    1er ecran de saisie fonctionnel

---
+++ gestFideles/src/controllers/AuthenticationServiceChapter3Impl.java
@@ -0,0 +1,42 @@
+/* 
+	Description:
+		ZK Essentials
+	History:
+		Created by dennis
+
+Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+*/
+package controllers;
+
+import java.io.Serializable;
+
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.Sessions;
+
+import services.AuthenticationService;
+import services.UserCredential;
+
+public class AuthenticationServiceChapter3Impl implements AuthenticationService,Serializable{
+	private static final long serialVersionUID = 1L;
+
+	public UserCredential getUserCredential(){
+		Session sess = Sessions.getCurrent();
+		UserCredential cre = (UserCredential)sess.getAttribute("userCredential");
+		if(cre==null){
+			cre = new UserCredential();//new a anonymous user and set to session
+			sess.setAttribute("userCredential",cre);
+		}
+		return cre;
+	}
+	
+
+	public boolean login(String nm, String pd) {
+		// will be implemented in chapter7
+		return false;
+	}
+
+
+	public void logout() {
+		// will be implemented in chapter7
+	}
+}

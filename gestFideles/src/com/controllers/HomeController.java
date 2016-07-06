44996a8a90725d0d164e312682139d3217a84218
Harold <haroldyeo@gmail.com>
2016-07-06 03:23:11 +0000

    reorganisation des fichiers - debut de travail sur l'authentification

---
+++ gestFideles/src/com/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package com.controllers;
+
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Window;
+
+
+public class HomeController extends SelectorComposer<Window>{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		
+	@Wire
+	Button btn;
+	
+	@Wire
+	Button abc;
+	
+	@Listen("onClick=#abc")
+	 public void clickButton(){
+		alert("hello");
+		 
+	 }
+	
+	
+
+}
---
+++ gestFideles/src/com/controllers/MainController.java
@@ -0,0 +1,94 @@
+package com.controllers;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.event.SerializableEventListener;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Grid;
+import org.zkoss.zul.Iframe;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.Rows;
+import org.zkoss.zul.Window;
+
+import com.services.SidebarPage;
+import com.services.SidebarPageConfig;
+
+public class MainController extends SelectorComposer<Component> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//wire components
+    @Wire Window winMain ;
+    @Wire Grid fnList;
+    @Wire Iframe mainFrame;
+
+  //services
+    SidebarPageConfig pageConfig = new SidebarPageConfigChapter2Impl();
+    
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+    	super.doAfterCompose(comp);
+    	
+    	 //initialize view after view construction.
+        
+        	Rows rows = fnList.getRows();
+
+            for(SidebarPage page:pageConfig.getPages()){
+                Row row = constructSidebarRow(page.getName(), page.getLabel(),page.getIconUri(),page.getUri());
+                rows.appendChild(row);
+            }
+            
+            Row row = constructSidebarRow("Yahoo", "Yahoo", null, "http://www.yahoo.fr");
+            rows.appendChild(row);
+            
+            Row row2 = constructSidebarRow("Page de connexion", "Page de connexion", null, "/index.zul");
+            rows.appendChild(row2);
+            
+            Row row3 = constructSidebarRow("Nouveau profil", "Nouveau profil", null, "/common/profile.zul");
+            rows.appendChild(row3);
+        }
+        
+    
+    
+    
+    private Row constructSidebarRow(String name,String label, String imageSrc, final String locationUri) {
+
+        //construct component and hierarchy
+        Row row = new Row();
+        Image image = new Image(imageSrc);
+        Label lab = new Label(label);
+
+        row.appendChild(image);
+        row.appendChild(lab);
+        row.setAttribute("link", locationUri);
+
+        //set style attribute
+        row.setSclass("sidebar-fn");
+        
+
+        //create and register event listener
+        EventListener<Event> actionListener = new SerializableEventListener<Event>() {
+            private static final long serialVersionUID = 1L;
+
+            public void onEvent(Event event) throws Exception {
+                //redirect current url to new location
+                mainFrame.setSrc(locationUri);
+            	
+            		
+            }
+        };
+
+        row.addEventListener(Events.ON_CLICK, actionListener);
+
+        return row;
+    }
+    
+}
